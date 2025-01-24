511cb2f2f5fca1b2c2b31d1a3f7e7de744bb0c86
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2025-01-24 11:16:24 +0100

    Added variables for region public/private subnets and cidr block

--- eks-cluster/variables.tf
+++ eks-cluster/variables.tf
@@ -16,3 +16,9 @@ variable "private_subnets" {
   type = list(string)
   default = [ "10.0.3.0/24", "10.0.4.0/24" ]
 }
+
+variable "aws_region" {
+  description = "AWS region for deployment"
+  type = string
+  default = "eu-west-2"
+}
