7fa8aadd2cf07df9843802306bab75c462deee20
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2025-01-24 12:31:41 +0100

    attached policies to the worker node IAM role

--- eks-cluster/eks.tf
+++ eks-cluster/eks.tf
@@ -40,3 +40,40 @@ resource "aws_eks_cluster" "jena_cluster" {
     Name = "jena-cluster"
   }
 }
+
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "jena-eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+
+  tags = {
+    Name = "jena-eks-node-role"
+  }
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_container_registry_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
