26ee73cd2847c329e1e8db4b171521dc7f4f1c96
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2025-01-24 11:57:33 +0100

    added elastic ip to the infra

--- eks-cluster/main.tf
+++ eks-cluster/main.tf
@@ -56,3 +56,10 @@ resource "aws_route_table_association" "public_subnet_association" {
   subnet_id = aws_subnet.public_subnets[count.index].id
   route_table_id = aws_route_table.public_rt.id
 }
+
+resource "aws_eip" "jena_nat_eip" {
+  domain = "vpc"
+  tags = {
+    Name = "jena-nat-eip"
+  }
+}
