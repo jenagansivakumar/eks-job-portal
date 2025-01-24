6083ff90c0b880c69a8212076cd813440c95c741
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2025-01-24 12:29:33 +0100

    Attached roles to the policy and controller

--- eks-cluster/eks.tf
+++ eks-cluster/eks.tf
@@ -16,3 +16,13 @@ resource "aws_iam_role" "eks_cluster_role" {
     Name = "jena-eks-cluster-role"
   }
 }
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_vpc_resource_controller" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
