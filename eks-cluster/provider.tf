a50ee2ba7776f308e74c3c39e39b620d4e426797
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2025-01-27 18:50:25 +0100

    reverted back

--- eks-cluster/provider.tf
+++ eks-cluster/provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.jena_cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.jena_cluster.certificate_authority.data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.jena_cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.jena.token
 }
 
